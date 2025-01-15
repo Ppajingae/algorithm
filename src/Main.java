a10cc9e1de6030fd0ebfdc8ac4c8b12c10485594
Ppajingae <wkmkyj991112@gmail.com>
2025-01-15 09:53:47 +0900

    20250115

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import algorithm.Algorithm250115;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+
+        Algorithm250115 algorithm250115 = new Algorithm250115();
+
+        System.out.println(algorithm250115.solution(6,3,2,1));
+
+
+        }
+}
---
+++ src/algorithm/Algorithm250115.java
@@ -0,0 +1,30 @@
+package algorithm;
+
+import java.util.*;
+
+public class Algorithm250115 {
+
+    public int solution(int a, int b, int c, int d) {
+
+        int[] tmpArr = {a,b,c,d};
+
+        Arrays.sort(tmpArr);
+
+        if(tmpArr[0] == tmpArr[3]) {
+            return  1111 * a;
+        }if(tmpArr[0] == tmpArr[2] || tmpArr[1] == tmpArr[3]){
+            return (int)Math.pow(tmpArr[1] * 10 + (tmpArr[0] + tmpArr[3] - tmpArr[1]), 2);
+        }if(tmpArr[0] == tmpArr[1] || tmpArr[1] == tmpArr[2] || tmpArr[2] == tmpArr[3]){
+            if(tmpArr[0] == tmpArr[1] && tmpArr[2] == tmpArr[3]){
+                return (tmpArr[0] + tmpArr[2]) * Math.abs(tmpArr[0] - tmpArr[2]);
+            }else{
+                if(tmpArr[0] == tmpArr[1]) return tmpArr[2] * tmpArr[3];
+                if(tmpArr[1] == tmpArr[2]) return tmpArr[0] * tmpArr[3];
+                return tmpArr[0] * tmpArr[1];
+            }
+        }else{
+            return tmpArr[0];
+        }
+
+    }
+}
