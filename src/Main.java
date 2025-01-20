e49074b00d0f5df23e1c2cd510ad14aec0716995
Ppajingae <wkmkyj991112@gmail.com>
2025-01-20 09:18:59 +0900

    250120

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,19 @@ import algorithm._250115.Algorithm250115;
 import algorithm._250116.Algorithm250116;
 import algorithm._250117.Algorithm250117;
 import algorithm._250118.Algorithm250118;
+import algorithm._250120.Algorithm250120;
+
+import java.util.Arrays;
 
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
 
-        Algorithm250118 algorithm250118 = new Algorithm250118();
+        Algorithm250120 algorithm250120 = new Algorithm250120();
 
 
-        int[] arr = {180, 120, 140};
-        int[] sides = {199, 72, 222};
-        System.out.println(algorithm250118.solution(arr, 190));
-        System.out.println(algorithm250118.triangle(sides));
+        int[] arr = {1, 3, 5,7};
+        System.out.println(Arrays.toString(algorithm250120.solution(arr)));
         }
 }
---
+++ src/algorithm/_250120/Algorithm250120.java
@@ -0,0 +1,19 @@
+package algorithm._250120;
+
+import java.util.Arrays;
+
+public class Algorithm250120 {
+
+    public int[] solution(int[] num_list) {
+        int[] answer = {0, 0};
+
+        for (int i : num_list){
+            if (i % 2 == 0){
+                answer[0]++;
+            }else answer[1]++;
+        }
+
+
+        return answer;
+    }
+}
